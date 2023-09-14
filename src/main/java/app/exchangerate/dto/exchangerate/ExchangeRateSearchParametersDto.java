4c944d5a497503f4ee524bc768a38c774bac306a
Olexii Melnik <alexmelnikk@gmail.com>
2023-09-14 15:04:37 +0300

    some change

--- src/main/java/app/exchangerate/dto/exchangerate/ExchangeRateSearchParametersDto.java
+++ src/main/java/app/exchangerate/dto/exchangerate/ExchangeRateSearchParametersDto.java
@@ -1,7 +1,8 @@
 package app.exchangerate.dto.exchangerate;
 
 import app.exchangerate.model.ExchangeRate;
+import java.time.LocalDateTime;
 
 public record ExchangeRateSearchParametersDto(
-        String [] localDates, ExchangeRate.Source [] sources) {
+        LocalDateTime [] localDates, ExchangeRate.Source [] sources) {
 }
